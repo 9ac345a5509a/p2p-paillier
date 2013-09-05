859a923af6ab116a4f2a6701d7010cc48c01721b
9ac345a5509a <9ac345a5509a@hotmail.com>
2013-09-05 12:22:44 -0400

    Code refactoring.

--- distribute.js
+++ distribute.js
@@ -1,6 +1,7 @@
 var firebaseMaster = new Firebase('http://paillier-cs.firebaseIO.com/');
 var myId;
 var awaitingConsensus = 0;
+var DEBUG = 1;
 var recId = [];
 var recSum = [];
 
@@ -16,17 +17,21 @@ function distributeAddition() {
 	if (awaitingConsensus == 1) {
 		return;
 	}
-	var decA = str2bigInt($('#inpA').val(), 10, bitLength, bitLength);
-	var decB = str2bigInt($('#inpB').val(), 10, bitLength, bitLength);
-	var cipherA = encryptInteger(decA, publicKey[0], publicKey[1]);
-	var cipherB = encryptInteger(decB, publicKey[0], publicKey[1]);
+	var decA = str2bigInt($('#inpA').val(), 10, 
+				paillier.bitLength, paillier.bitLength);
+	var decB = str2bigInt($('#inpB').val(), 10, 
+				paillier.bitLength, paillier.bitLength);
+	var cipherA = paillier.encryptInteger(decA);
+	var cipherB = paillier.encryptInteger(decB);
 	myId = makeId();
-	addToLog('[' + strip6(myId) + '] ');
+	if (DEBUG) {
+		addToLog('[' + strip6(myId) + '] ');
 		addToLog('Requested multiplication of ');
 		addBigIntToLog(cipherA);
 		addToLog(' and ');
 		addBigIntToLog(cipherB);
 		addToLog('\n');
+	}
 	recSum = [];
 	awaitingConsensus = 1;
 	firebaseMaster.push({action: 'add', 
@@ -34,16 +39,20 @@ function distributeAddition() {
 						 id: myId,
 						 cA: bigInt2str(cipherA, 2),
 						 cB: bigInt2str(cipherB, 2),
-						 n:  bigInt2str(publicKey[0], 2)});
-	$('#outC').val('Awaiting network consensus...');
-	$('#outC').addClass('btn-inverse');
-	$('#inpA, #inpB').attr('disabled', '');
+						 n:  bigInt2str(paillier.n, 2)});
+	if (DEBUG) {
+		$('#outC').val('Awaiting network consensus...');
+		$('#outC').addClass('btn-inverse');
+		$('#inpA, #inpB').attr('disabled', '');
+	}
 	setTimeout(getConsensus, 5000);
 }
 
 var messageQuery = firebaseMaster.endAt().limit(10);
 
-addToLog('Joining the network as a peer.\n');
+if (DEBUG) {
+	addToLog('Joining the network as a peer.\n');
+}
 
 messageQuery.on('child_added', function(snapshot) {
 	var message = snapshot.val();
@@ -54,13 +63,16 @@ messageQuery.on('child_added', function(snapshot) {
 		var cA = str2bigInt(message.cA, 2, message.cA.length, message.cA.length);
 		var cB = str2bigInt(message.cB, 2, message.cB.length, message.cB.length);
 		var rN = str2bigInt(message.n , 2, message.n.length,  message.n.length);
-		var sum= addEncIntegers(cA, cB, rN);
-		addToLog('[' + strip6(message.id) + '] ');
+		var sum= paillier.addEncIntegers(cA, cB, rN);
+
+		if (DEBUG) {
+			addToLog('[' + strip6(message.id) + '] ');
 			addToLog('Multiplied ');
 			addToLog('cA = '); addBigIntToLog(cA);
 			addToLog('; cB = '); addBigIntToLog(cB);
 			addToLog('; cA * cB = '); addBigIntToLog(sum);
 			addToLog('\n');
+		}
 
 		firebaseMaster.push({
 			action: 'send',
@@ -90,10 +102,11 @@ function getConsensus() {
 		}
 	}
 	awaitingConsensus = 0;
-	$('#outC').val(bigInt2str(
-		decryptRecAns(popularSum, publicKey[0],
-			privateKey[0], privateKey[1]),
-		10));
-	$('#outC').removeClass('btn-inverse');
-	$('#inpA, #inpB').removeAttr('disabled');
+	if (DEBUG) {
+		$('#outC').val(bigInt2str(
+			paillier.decryptRecAns(popularSum),
+			10));
+		$('#outC').removeClass('btn-inverse');
+		$('#inpA, #inpB').removeAttr('disabled');
+	}
 }
